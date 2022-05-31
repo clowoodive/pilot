e35674a7a08ea78189159d3b58aaa1a892131aa6
clowoodive <clowoodive@gmail.com>
2022-05-31 18:10:39 +0900

    Feat: Add pilot-spring-cloud-config-server project

---
+++ pilot-spring-cloud-config-server/src/main/java/clowoodive/pilot/springcloudconfigserver/SpringcloudconfigserverApplication.java
@@ -0,0 +1,13 @@
+package clowoodive.pilot.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringcloudconfigserverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudconfigserverApplication.class, args);
+	}
+}
---
+++ pilot-spring-cloud-config-server/src/test/java/clowoodive/pilot/springcloudconfigserver/SpringcloudconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package clowoodive.pilot.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
