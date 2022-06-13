ae0bd2c06f27998d7cd2372cb7703ccc4773fd6e
clowoodive <clowoodive@gmail.com>
2022-06-13 15:57:43 +0900

    Refactor: Remove json test code

--- pilot-resttemplate/README.md
+++ pilot-resttemplate/README.md
@@ -1,6 +1,1 @@
-## RestTemplate 맛보기
- - restTemplateRunner
-
-## RestTemplate의 json 응답을 OffsetDateTime으로 deserialize 하기
- - deserializeRunner
- - domain과 clientId를 비공개 시켰으므로 바로 실행 불가
+### RestTemplate 기본 동작 확인
--- pilot-resttemplate/src/main/java/clowoodive/pilot/resttemplate/RestTemplateApplication.java
+++ pilot-resttemplate/src/main/java/clowoodive/pilot/resttemplate/RestTemplateApplication.java
@@ -1,19 +1,14 @@
 package clowoodive.pilot.resttemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.*;
+import java.time.Duration;
 
 @SpringBootApplication
 public class RestTemplateApplication {
@@ -25,8 +20,7 @@ public class RestTemplateApplication {
         SpringApplication.run(RestTemplateApplication.class, args);
     }
 
-    // 최초 RestTemplete 테스트
-//    @Bean
+    @Bean
     public ApplicationRunner restTemplateRunner() {
         return args -> {
 //			RestTemplate restTemplate = restTemplateBuilder.build();
@@ -39,36 +33,6 @@ public class RestTemplateApplication {
 //			Arrays.stream(results).forEach(r -> {
 //				System.out.println("repo name:" + r.getName());
 //			});
-
         };
     }
-
-    // Jackson2ObjectMapperBuilder 설정과 json의 OffsetDateTime을 deserialize 하는 테스트
-    @Bean
-    public ApplicationRunner deserializeRunner(ObjectMapper objectMapper) {
-        return args -> {
-            var domain = "secret";
-            var clientId = "secret";
-
-            var zonedExpireAt = ZonedDateTime.of(LocalDateTime.now().toLocalDate(), LocalTime.of(11, 22, 33), ZoneOffset.ofHours(9));
-
-            System.out.println(zonedExpireAt);
-
-            var headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-
-            var reqBodyProto = new RestTemplateProto.Req();
-            reqBodyProto.setClientId(clientId);
-            reqBodyProto.setUserIdentifier(String.valueOf(123456));
-            reqBodyProto.setIssueCategory("test");
-            reqBodyProto.setExpireAt(zonedExpireAt);
-
-            var body = objectMapper.writeValueAsString(reqBodyProto);
-
-            var restTemplate = new RestTemplate();
-            var res = restTemplate.exchange(domain, HttpMethod.POST, new HttpEntity<String>(body, headers), RestTemplateProto.Res.class);
-            System.out.println(res.getBody().getExpireAt());
-        };
-    }
-
 }
